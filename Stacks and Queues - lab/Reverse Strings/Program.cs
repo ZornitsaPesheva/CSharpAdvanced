30c206c1b78fd39010cef380c3a68ce1c0f1f3f3
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-05-28 15:29:05 +0300

    Stacks and Queues

---
+++ Stacks and Queues - lab/Reverse Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Stacks and Queues/1. Reverse Numbers with a Stack/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Reverse_Numbers_with_a_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Trim()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            foreach (var number in numbers)
+            {
+                stack.Push(number);
+            }
+
+            Console.Write(string.Join(" ", stack));
+            //foreach (var item in stack.ToArray())
+            //{
+            //    Console.WriteLine(stack.Pop());
+            //}
+            Console.ReadLine();
+        }
+    }
+}

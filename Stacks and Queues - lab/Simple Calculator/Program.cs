0438d381fbdcd2cc3c1e089c46b7b8df6bb9c3c8
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-05-29 11:27:15 +0300

    Simple Calculator

---
+++ Stacks and Queues - lab/Simple Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simple_Calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var reminder = input.Split(' ');
+            var stack = new Stack<string>(reminder.Reverse());
+
+            while (stack.Count > 1)
+            {
+                var firstNumber = int.Parse(stack.Pop());
+                var op = stack.Pop();
+                var secondNumber = int.Parse(stack.Pop());
+
+                if (op == "+")
+                {
+                    stack.Push((firstNumber + secondNumber).ToString());
+                }
+                else
+                {
+                    stack.Push((firstNumber - secondNumber).ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}

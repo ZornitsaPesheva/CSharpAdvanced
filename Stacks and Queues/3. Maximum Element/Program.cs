3c4f925e25ed2c610ab98f9e3b9b6a5b3c1a214a
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-06-04 19:46:20 +0300

    max value

--- Stacks and Queues/3. Maximum Element/Program.cs
+++ Stacks and Queues/3. Maximum Element/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _3.Maximum_Element
 {
@@ -10,6 +8,49 @@ namespace _3.Maximum_Element
     {
         static void Main(string[] args)
         {
+            int queriesNumber = int.Parse(Console.ReadLine());
+
+            var numbers = new Stack<int>();
+
+            var maxNumbers = new Stack<int>();
+            var maxValue = int.MinValue;
+
+            for (int i = 0; i < queriesNumber; i++)
+            {
+                var query = Console.ReadLine()
+                    .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (query[0] == 1)
+                {
+                    numbers.Push(query[1]);
+                    if (maxValue < query[1])
+                    {
+                        maxValue = query[1];
+                        maxNumbers.Push(maxValue);
+                    }
+                }
+                else if (query[0] == 2)
+                {
+                    if (numbers.Pop() == maxValue)
+                    {
+                        maxNumbers.Pop();
+                        if (maxNumbers.Count() != 0)
+                        {
+                            maxValue = maxNumbers.Peek();
+                        }
+                        else
+                        {
+                            maxValue = int.MinValue;
+                        }
+                    }
+                }
+                else if (query[0] == 3)
+                {
+                    Console.WriteLine(maxValue);
+                }
+            }
         }
     }
 }

b367a701f6976ffc8bbed7083bc7fa134b43d0f9
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-06-05 14:55:32 +0300

    sequence with gueue

---
+++ Stacks and Queues/5. Sequence with Queue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.Sequence_with_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            Queue<long> queue = new Queue<long>();
+            Queue<long> queue1 = new Queue<long>();
+
+            queue.Enqueue(n);
+            long s1 = queue.Peek();
+
+            while (queue.Count < 50)
+            {
+                queue.Enqueue(s1 + 1);
+                queue1.Enqueue(s1 + 1);
+                long sx = queue1.Dequeue();
+                if (queue.Count < 50)
+                {
+                    queue.Enqueue(2 * s1 + 1);
+                    queue1.Enqueue(2 * s1 + 1);
+                }               
+
+                if (queue.Count < 50)
+                {
+                    queue.Enqueue(s1 + 2);
+                    queue1.Enqueue(s1 + 2);
+                }                
+                
+                s1 = sx;
+            }
+
+            Console.Write(string.Join(" ", queue));
+
+        }
+    }
+}

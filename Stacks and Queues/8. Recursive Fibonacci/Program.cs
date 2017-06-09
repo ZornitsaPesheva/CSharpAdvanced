997c85536778872d5ec5240c203f0a2526613a83
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-06-09 16:54:01 +0300

    Recursive Fibonacci

--- Stacks and Queues/8. Recursive Fibonacci/Program.cs
+++ Stacks and Queues/8. Recursive Fibonacci/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _8.Recursive_Fibonacci
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static long[] fibNumbers;
+
+        public static void Main(string[] args)
         {
+            int nthNumber = int.Parse(Console.ReadLine());
+            fibNumbers = new long[nthNumber];
+            var result = GetFibonacci(nthNumber);
+            Console.WriteLine(result);
+        }
+
+        private static long GetFibonacci(int nthNumber)
+        {
+            if (nthNumber <= 2)
+            {
+                return 1;
+            }
+            if (fibNumbers[nthNumber - 1] != 0)
+            {
+                return fibNumbers[nthNumber - 1];
+            }
+
+            return fibNumbers[nthNumber - 1] = GetFibonacci(nthNumber - 1) + GetFibonacci(nthNumber - 2);
         }
     }
 }

f0efdad2b783b9903ce9119d0ccb12210742a8e1
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-06-16 14:11:15 +0300

    Parking Lot

---
+++ Sets and Dictionaries - Lab/ParkingLot/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var parking = new SortedSet<string>();
+
+            while (input != "END")
+            {
+                var inputParams = Regex.Split(input, ", ");
+
+                if (inputParams[0] == "IN")
+                {
+                    parking.Add(inputParams[1]);
+                }
+                else
+                {
+                    if (parking.Contains(inputParams[1]))
+                    {
+                        parking.Remove(inputParams[1]);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (parking.Count != 0)
+            {
+                foreach (var car in parking)
+                {
+                    Console.WriteLine(car.Trim());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+      
+        }
+    }
+}

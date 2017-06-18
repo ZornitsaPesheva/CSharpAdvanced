3f10fa02556198a0f2692d6478b3e10460c3bf18
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-06-18 17:54:16 +0300

    Count Same ValuesIn Array

--- Sets and Dictionaries - Lab/Count Same Values In Array/Program.cs
+++ Sets and Dictionaries - Lab/Count Same Values In Array/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Count_Same_Values_In_Array
 {
@@ -8,46 +9,27 @@ namespace Count_Same_Values_In_Array
         static void Main(string[] args)
         {
             var input = Console.ReadLine();
-            var inputTokens = input.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var inputTokens = input.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse);
 
             var dikt = new SortedDictionary<double, int>();
 
             foreach (var token in inputTokens)
             {
-                double reminder;
-                if (token.Contains(","))
+
+                if (!dikt.ContainsKey(token))
                 {
-                    reminder = double.Parse(token.Replace(",", "."));
+                    dikt.Add(token, 1);
                 }
                 else
                 {
-                    reminder = double.Parse(token);
-                }
-
-
-
-                if (!dikt.ContainsKey(reminder))
-                {
-                    dikt.Add(reminder, 1);
-                }
-                else
-                {
-                    dikt[reminder]++;
+                    dikt[token]++;
                 }
             }
 
             foreach (var pair in dikt)
             {
-                if (pair.Key.ToString().Contains("."))
-                {
-                    var reminder = pair.Key.ToString().Replace(",", ",");
-                    Console.WriteLine($"{reminder} - {pair.Value} times");
-                }
-                else
-                {
-                    Console.WriteLine($"{pair.Key} - {pair.Value} times");
-                }
-
+                 Console.WriteLine($"{pair.Key} - {pair.Value} times");
             }
         }
     }

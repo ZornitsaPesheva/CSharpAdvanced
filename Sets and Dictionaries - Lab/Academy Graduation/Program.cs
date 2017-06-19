5e56afe45bdfbe518d896d5610f893aa774a525c
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-06-19 11:53:48 +0300

    Academy Graduation

--- Sets and Dictionaries - Lab/Academy Graduation/Program.cs
+++ Sets and Dictionaries - Lab/Academy Graduation/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Academy_Graduation
 {
@@ -10,6 +9,21 @@ namespace Academy_Graduation
     {
         static void Main(string[] args)
         {
+            var number = int.Parse(Console.ReadLine());
+            var students = new SortedDictionary<string, List<double>>();
+            for (int i = 0; i < number; i++)
+            {
+                var student = Console.ReadLine();
+                var results = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(n => double.Parse(n, CultureInfo.InvariantCulture))
+                    .ToList();
+                students.Add(student, results);
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Key} is graduated with {student.Value.Average()}");
+            }
         }
     }
 }

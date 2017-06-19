783417726973fb160c0226733c4e17063d49a63b
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-06-19 17:12:23 +0300

    Sets and Dictionaries - Exercise

---
+++ Sets and Dictionaries - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            var set = new HashSet<string>();
+            for (int i = 1; i <= number; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+            }
+
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries - Exercise/02. Sets Of Elements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Sets_Of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sets = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = sets[0];
+            int m = sets[1];
+
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                set1.Add(num);
+            }
+
+            for (int i = 1; i <= m; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (set1.Contains(num))
+                {
+                    set2.Add(num);
+                }
+                
+            }
+
+            Console.WriteLine(string.Join(" ", set2));
+        }
+    }
+}

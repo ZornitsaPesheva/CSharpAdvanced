83426f920eb70d4b6af83725c41a7f50edfc8f5d
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-06-20 11:35:15 +0300

    Count Symbols

---
+++ Sets and Dictionaries - Exercise/04. Count Symbols/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            SortedDictionary<char, int> dict = new SortedDictionary<char, int>();
+           
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!dict.ContainsKey(text[i]))
+                {
+                    dict.Add(text[i], 1);
+                }
+                else
+                {
+                    dict[text[i]]++;
+                }
+                
+            }
+
+            foreach (var symbol in dict)
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+
+            }
+
+        }
+    }
+}

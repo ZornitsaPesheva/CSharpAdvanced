f8ad4d493190de2530b18d7242c4247d3c790f86
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-06-20 21:46:15 +0300

    User Logs

--- Sets and Dictionaries - Exercise/09. User Logs/Program.cs
+++ Sets and Dictionaries - Exercise/09. User Logs/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _09.User_Logs
 {
@@ -10,6 +7,56 @@ namespace _09.User_Logs
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
+
+            SortedDictionary<string, HashSet<string>> ips = new SortedDictionary<string, HashSet<string>>();
+            Dictionary<string, int> msgs = new Dictionary<string, int>();
+            
+            while (input != "end")
+            {
+                string[] param = input.Split(' ');
+
+                string ip = (param[0].Split('='))[1];
+              //  string message = (param[1].Split('='))[1];
+                string user = (param[2].Split('='))[1];
+
+                if (!ips.ContainsKey(user))
+                {
+                    HashSet<string> ipsList = new HashSet<string>();
+                    ipsList.Add(ip);
+                    ips.Add(user, ipsList);
+                }
+                else
+                {
+                    ips[user].Add(ip);                  
+                }
+
+                if (!msgs.ContainsKey(ip))
+                {
+                    msgs.Add(ip, 1);
+                }
+                else
+                {
+                    msgs[ip]++;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var usr in ips)
+            {
+                Console.WriteLine(usr.Key + ":");
+
+                HashSet<string> result = new HashSet<string>();
+
+                foreach (var uip in usr.Value)
+                {
+                    result.Add(uip + " => " + msgs[uip]);
+                    // Console.WriteLine($"{uip} => {msgs[uip]},");
+                }
+
+                Console.WriteLine(string.Join(", ", result) + ".");
+            }
         }
     }
 }

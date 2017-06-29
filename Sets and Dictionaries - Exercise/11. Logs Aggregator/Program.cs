46ba4bd53e5b02aa1a965c43543cda313bb5e159
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-06-29 10:34:00 +0300

    Logs Aggregator

--- Sets and Dictionaries - Exercise/11. Logs Aggregator/Program.cs
+++ Sets and Dictionaries - Exercise/11. Logs Aggregator/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _11.Logs_Aggregator
 {
@@ -10,6 +8,47 @@ namespace _11.Logs_Aggregator
     {
         static void Main(string[] args)
         {
+
+            int numberOfLogs = int.Parse(Console.ReadLine());
+
+            SortedDictionary<string, SortedDictionary<string, int>> users = 
+                new SortedDictionary<string, SortedDictionary<string, int>>();
+            
+            for (int i = 1; i <= numberOfLogs; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ');
+
+                string user = input[1];
+                string ip = input[0];
+                int duration = int.Parse(input[2]);
+
+                if (!users.ContainsKey(user))
+                {
+                    SortedDictionary<string, int> ipDictionary = 
+                        new SortedDictionary<string, int>();
+                    ipDictionary.Add(ip, duration);
+
+                    users.Add(user, ipDictionary);
+                }
+                else
+                {
+                    if (!users[user].ContainsKey(ip))
+                    {
+                        users[user].Add(ip, duration);
+                    }
+                    else
+                    {
+                        users[user][ip] += duration;
+                    }
+                }
+            }
+
+            foreach (var u in users)
+            {
+                Console.WriteLine($"{u.Key}: {u.Value.Values.Sum()} [" +
+                                  $"{String.Join(", ", u.Value.Keys)}]");
+            }
         }
     }
 }
